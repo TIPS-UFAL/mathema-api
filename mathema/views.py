fc0a098aaf655f0ddb490cb01f8ff8a5015ac9b2
Gabriel <gbp@ic.ufal.br>
2019-06-19 14:40:34 -0300

    Updates views.py (stopped on Topic)

--- mathema/views.py
+++ mathema/views.py
@@ -1,14 +1,11 @@
-# from django.shortcuts import render #padrao
 from django.shortcuts import get_object_or_404
-# from django.db.migrations import serializer
-from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters, generics, mixins
 from django.contrib.auth.models import User
 
-from .models import *
-from .models import Topic as TopicModel, \
-    StudentGroup as StudentGroupModel, Group as GroupModel, Question as QuestionModel, ModelSolution as MSModel, ProposedSolution as PSModel, Feedback as FeedbackModel, StudentModel as SMModel
+from .models import Topic as TopicModel, TopicCurriculum as TCModel, \
+    StudentGroup as StudentGroupModel, Group as GroupModel, Question as QuestionModel, ModelSolution as MSModel, \
+    ProposedSolution as PSModel, Feedback as FeedbackModel, StudentModel as SMModel
 from .serializers import *
 from filters.mixins import (FiltersMixin, )
 from rest_framework.permissions import IsAuthenticated
@@ -18,19 +15,19 @@ from .permissions import IsTeacher, IsOwnerOrReadOnly
     APIREST
 """
 
-"""
-    List all curriculuns (or a number of curriculuns), or create a new one.
-    # api/curriculum
-    Retrieve, up  date or delete a curriculum instance.
-    # api/curriculum/:pk
-    Search curriculuns by title
-    # api/curriculum?title='' or api/curriculum?search=''
-"""
-
 
 class Tag(FiltersMixin, viewsets.GenericViewSet,
-          mixins.ListModelMixin):
-    permission_classes = (IsAuthenticated, IsTeacher,)
+          mixins.ListModelMixin,
+          mixins.RetrieveModelMixin):
+    """
+        List all curriculums (or a number of curriculuns), or create a new one.
+        # api/curriculum
+        Retrieve, up  date or delete a curriculum instance.
+        # api/curriculum/:pk
+        Search curriculuns by title
+        # api/curriculum?title='' or api/curriculum?search=''
+    """
+    permission_classes = (IsAuthenticated,)
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
@@ -42,17 +39,15 @@ class Tag(FiltersMixin, viewsets.GenericViewSet,
     }
 
 
-"""
-    List all curriculuns (or a number of curriculuns), or create a new one.
-    # api/curriculum
-    Retrieve, up  date or delete a curriculum instance.
-    # api/curriculum/:pk
-    Search curriculuns by title
-    # api/curriculum?title='' or api/curriculum?search=''
-"""
-
-
 class Curriculum(FiltersMixin, viewsets.ModelViewSet):
+    """
+        List all curriculums (or a number of curriculuns), or create a new one.
+        # api/curriculum
+        Retrieve, up  date or delete a curriculum instance.
+        # api/curriculum/:pk
+        Search curriculuns by title
+        # api/curriculum?title='' or api/curriculum?search=''
+    """
     permission_classes = (IsAuthenticated, IsTeacher,)
 
     queryset = Curriculum.objects.all().order_by('-id')
@@ -62,17 +57,8 @@ class Curriculum(FiltersMixin, viewsets.ModelViewSet):
     search_fields = ('title',)
     filter_mappings = {
         'title': 'title',
+        'author': 'author'
     }
-
-
-"""
-    List all Group, or create a new Group.
-    # api/group/
-    Retrieve, update or delete a Group instance.
-    # api/group/:id/
-    Search groups by group_key
-    # api/group?group_key='' or api/group?search=''
-"""
 
 
 class Group(viewsets.GenericViewSet,
@@ -80,6 +66,14 @@ class Group(viewsets.GenericViewSet,
             mixins.CreateModelMixin,
             mixins.UpdateModelMixin,
             mixins.DestroyModelMixin):
+    """
+        List all Group, or create a new Group.
+        # api/group/
+        Retrieve, update or delete a Group instance.
+        # api/group/:id/
+        Search groups by group_key
+        # api/group?group_key='' or api/group?search=''
+    """
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
     queryset = GroupModel.objects.all()
@@ -90,21 +84,53 @@ class Group(viewsets.GenericViewSet,
         serializer = GroupSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('group_key',)
+    filter_mappings = {
+        'name': 'name',
+        'group_key': 'group_key',
+        'teacher': 'teacher'
+    }
 
-"""
-    List all Activity, or create a new Activity.
-    # api/activity/
-    Retrieve, update or delete a Activity instance.
-    # api/activity/:id/
-"""
+
+class StudentGroup(FiltersMixin,
+                   viewsets.GenericViewSet,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin):
+    """
+        List all StudentGroup, or create a new StudentGroup.
+        # api/studentGroup/
+        Retrieve, update or delete a StudentGroup instance.
+        # api/studentGroup/:id_student/:id_group
+    """
+    queryset = StudentGroupModel.objects.all().order_by('-id')
+    serializer_class = StudentGroupSerializer
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            permission_classes = [IsTeacher, IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+    def retrieve(self, request, pk_student=None, pk_group=None):
+        queryset = StudentGroupModel.objects.filter(student=pk_student, group=pk_group)
+        group = get_object_or_404(self.queryset, pk=queryset)
+        serializer = StudentGroupSerializer(group)
+        return Response(serializer.data)
 
 
-class Question(viewsets.GenericViewSet,
-               mixins.CreateModelMixin,
-               mixins.DestroyModelMixin,
-               mixins.UpdateModelMixin,
-               mixins.RetrieveModelMixin):
-    queryset = QuestionModel.objects.all()
+class Question(FiltersMixin, viewsets.ModelViewSet):
+    """
+        List all Activity, or create a new Activity.
+        # api/activity/
+        Retrieve, update or delete a Activity instance.
+        # api/activity/:id/
+    """
+    # Todo: filtrar se é privado ou não no caso de retrieve e list, testar se colocando aqui, o create, o destroy e o
+    #  update ficam normal
+    queryset = QuestionModel.objects.all().order_by('difficulty_level')
     serializer_class = QuestionSerializer
 
     def get_permissions(self):
@@ -117,37 +143,57 @@ class Question(viewsets.GenericViewSet,
 
         return [permission() for permission in permission_classes]
 
-    def list(self, request, pk_tag):
-        queryset = QuestionModel.objects.filter(tag=pk_tag)
-        serializer = QuestionSerializer(queryset, many=True)
-        return Response(serializer.data)
-
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('title', 'difficulty_level')
+    search_fields = ('title', 'main_tag', 'tags')
     filter_mappings = {
         'title': 'title',
         'difficulty_level': 'difficulty_level',
+        'main_tag': 'main_tag',
+        'tags': 'tags'
     }
 
 
-"""
-    List all Topic, or create a new Topic.
-    # api/topic/
-    Retrieve, update or delete a Topic instance.
-    # api/topic/:id/
-"""
+class TopicCurriculum(FiltersMixin, viewsets.GenericViewSet,
+                      mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.RetrieveModelMixin):
+    """
+        List all Topic, or create a new Topic.
+        # api/topic/
+        Retrieve, update or delete a Topic instance.
+        # api/topic/:id/
+    """
+    queryset = TCModel.objects.all()
+    serializer_class = TopicSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsTeacher, IsAuthenticated]
+        elif self.action == 'update' or self.action == 'destroy':
+            permission_classes = [IsTeacher, IsOwnerOrReadOnly, IsAuthenticated]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
 
 
-class Topic(FiltersMixin, viewsets.GenericViewSet,
+class Topic(viewsets.GenericViewSet,
             mixins.CreateModelMixin,
             mixins.DestroyModelMixin,
             mixins.UpdateModelMixin,
             mixins.RetrieveModelMixin):
+    """
+        List all Topic, or create a new Topic.
+        # api/topic/
+        Retrieve, update or delete a Topic instance.
+        # api/topic/:id/
+    """
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
 
     def list(self, request, pk_curriculum):
-        queryset = TopicModel.objects.filter(curriculum=pk_curriculum)
+        queryset = TopicModel.objects.filter(curriculum=pk_curriculum).order_by('curriculum.sequence')
         serializer = TopicSerializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -162,24 +208,19 @@ class Topic(FiltersMixin, viewsets.GenericViewSet,
         return [permission() for permission in permission_classes]
 
 
-#     filter_backends = (filters.SearchFilter,)
-#     search_fields = ('titulo',)
-#     filter_mappings = {
-#         'title': 'titulo',
-#     }
-
-
 """
     List all StudentGroup, or create a new StudentGroup.
     # api/studentGroup/
     Retrieve, update or delete a StudentGroup instance.
     # api/studentGroup/:id_student/:id_group
 """
+
+
 class ModelSolution(FiltersMixin, viewsets.GenericViewSet,
-            mixins.CreateModelMixin,
-            mixins.DestroyModelMixin,
-            mixins.UpdateModelMixin,
-            mixins.RetrieveModelMixin):
+                    mixins.CreateModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.RetrieveModelMixin):
     queryset = MSModel.objects.all()
     serializer_class = ModelSolutionSerializer
 
@@ -199,7 +240,6 @@ class ModelSolution(FiltersMixin, viewsets.GenericViewSet,
         return [permission() for permission in permission_classes]
 
 
-
 """
     List all StudentGroup, or create a new StudentGroup.
     # api/studentGroup/
@@ -209,10 +249,10 @@ class ModelSolution(FiltersMixin, viewsets.GenericViewSet,
 
 
 class ProposedSolution(FiltersMixin, viewsets.GenericViewSet,
-            mixins.CreateModelMixin,
-            mixins.DestroyModelMixin,
-            mixins.UpdateModelMixin,
-            mixins.RetrieveModelMixin):
+                       mixins.CreateModelMixin,
+                       mixins.DestroyModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.RetrieveModelMixin):
     queryset = PSModel.objects.all()
     serializer_class = ProposedSolutionSerializer
 
@@ -236,11 +276,13 @@ class ProposedSolution(FiltersMixin, viewsets.GenericViewSet,
     Retrieve, update or delete a StudentGroup instance.
     # api/studentGroup/:id_student/:id_group
 """
+
+
 class Feedback(FiltersMixin, viewsets.GenericViewSet,
-            mixins.CreateModelMixin,
-            mixins.DestroyModelMixin,
-            mixins.UpdateModelMixin,
-            mixins.RetrieveModelMixin):
+               mixins.CreateModelMixin,
+               mixins.DestroyModelMixin,
+               mixins.UpdateModelMixin,
+               mixins.RetrieveModelMixin):
     queryset = FeedbackModel.objects.all()
     serializer_class = FeedbackSerializer
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
@@ -248,39 +290,6 @@ class Feedback(FiltersMixin, viewsets.GenericViewSet,
     def list(self, request):
         queryset = FeedbackModel.objects.filter()
         serializer = FeedbackSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
-
-
-
-"""
-    List all StudentGroup, or create a new StudentGroup.
-    # api/studentGroup/
-    Retrieve, update or delete a StudentGroup instance.
-    # api/studentGroup/:id_student/:id_group
-"""
-
-
-class StudentGroup(FiltersMixin,
-                   viewsets.GenericViewSet,
-                   mixins.CreateModelMixin,
-                   mixins.DestroyModelMixin):
-    queryset = StudentGroupModel.objects.all().order_by('-id')
-    serializer_class = StudentGroupSerializer
-
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            permission_classes = [IsTeacher, IsAuthenticated]
-        else:
-            permission_classes = [IsAuthenticated]
-
-        return [permission() for permission in permission_classes]
-
-    def retrieve(self, request, pk_student=None, pk_group=None):
-        queryset = StudentGroupModel.objects.filter(student=pk_student, group=pk_group)
-        group = get_object_or_404(self.queryset, pk=queryset)
-        serializer = StudentGroupSerializer(group)
         return Response(serializer.data)
 
 
@@ -293,9 +302,6 @@ class StudentGroup(FiltersMixin,
 # class Objective(FiltersMixin, viewsets.ModelViewSet):
 #     queryset = Objective.objects.all().order_by('-id')
 #     serializer_class = ObjectiveSerializer
-
-
-
 
 
 """
