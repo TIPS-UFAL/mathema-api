9aecd2635259dab834a5a4e1114a2e60e7df5107
Gabriel <gbp@ic.ufal.br>
2019-06-27 15:03:15 -0300

    Adds new models on admin page

--- mathema/admin.py
+++ mathema/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
-from .models import Curriculum, Topic, Tag, Question, TopicCurriculum, Topic, QuestionTopic, ModelSolution, \
+from .models import Curriculum, Tag, Question, TopicCurriculum, Topic, QuestionTopic, ModelSolution, \
     User, Group, StudentGroup, ProposedSolution, Feedback, StudentModel
 
 admin.site.register(User)
+admin.site.register(Tag)
 admin.site.register(Curriculum)
-admin.site.register(Topic)
 admin.site.register(Group)
 admin.site.register(StudentGroup)
+admin.site.register(Question)
+admin.site.register(TopicCurriculum)
+admin.site.register(Topic)
+admin.site.register(QuestionTopic)
+admin.site.register(ModelSolution)
+admin.site.register(ProposedSolution)
+admin.site.register(Feedback)
+admin.site.register(StudentModel)
 
